0fee56e36f0a0e54dd273f93c9b9510f078cd143
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-06-11 22:26:50 +0200

    Ajout key dans slider et clean code

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -18,40 +18,39 @@ const Slider = () => {
     return () => clearInterval(interval);
   }, [byDateDesc]);
 
-  return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-           key={`SlideCard_${event.title}`}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
+    return (
+      <div className="SlideCardList">
+        {byDateDesc?.map((event, idx) => (
+          <>
+            <div key={`SlideCard_${event.title}`}
+              className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
+                }`}
+            >
+              <img src={event.cover} alt="forum" />
+              <div className="SlideCard__descriptionContainer">
+                <div className="SlideCard__description">
+                  <h3>{event.title}</h3>
+                  <p>{event.description}</p>
+                  <div>{getMonth(new Date(event.date))}</div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
+            <div key={`SliderPaginationContainer_${event.id}`} className="SlideCard__paginationContainer">
+              <div className="SlideCard__pagination" name="pagination">
+                {byDateDesc.map((_, radioIdx) => (
+                  <input
+                    type="radio"
+                    name="radio-button"
+                    value={radioIdx}
+                   /*  ici : peut-être changer la nature des inputs ??    */
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-        </>
-      ))}
-    </div>
-  );
-};
+          </>
+        ))}
+      </div>
+    );
+  };
 
-export default Slider;
+  export default Slider;
